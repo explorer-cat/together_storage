8750321504a7bf3cb87d9ece0bb7d5090f5eca50
sungwoo choi <sqlstyle@naver.com>
2022-03-28 21:19:56 +0900

    together_storage

---
+++ src/components/category.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './category.css';
+
+
+/*전체 카테고리 레이아웃*/
+function Category() {
+  return (
+    <div className="category-layout">
+        
+    </div>
+  );
+}
+
+export default Category;
